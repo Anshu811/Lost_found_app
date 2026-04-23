a7d6d7f5d45249d68b1c765929966126f17da9ce
Anshu <anshukumari889900@gmail.com>
2026-04-23 10:58:52 +0530

    ready for render deploy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const itemRoutes = require('./routes/items');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
